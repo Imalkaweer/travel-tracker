c43c5b311759924d15e1f15fc1c1161f7a9b53ec
imalkaweerasekara <imajavaweerasekara128@gmail.com>
2024-12-24 18:25:51 +0530

    added sorting capabilities

--- src/App.jsx
+++ src/App.jsx
@@ -72,10 +72,28 @@ function Form({ onAddItems }) {
 }
 
 function PackageList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItemList;
+
+  if (sortBy === "input") sortedItemList = items;
+
+  if (sortBy === "description")
+    sortedItemList = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed") {
+    console.log("hi");
+    sortedItemList = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+    console.log(sortedItemList);
+  }
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItemList.map((item) => (
           <Item
             item={item}
             onDeleteItem={onDeleteItem}
@@ -84,6 +102,14 @@ function PackageList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        <button>Clear list</button>
+      </div>
     </div>
   );
 }
